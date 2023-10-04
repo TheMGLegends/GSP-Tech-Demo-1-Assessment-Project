cce4f9b516b8d6da26ce8ea9a98a4a563564462c
TheMGLegends <matejovo34@gmail.com>
2023-10-04 18:18:56 +0100

    Implemented Objectives 1 & 2
    Implemented Obj 1:
    1. 2D Platforming Mechanics (Move & Jump)
    
    Implemented Obj 2:
    2. Fully Animated Main Character (Idle, Run, Jump and Fall)

---
+++ Tech Demo 1/Assets/_Scripts/FollowObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    [SerializeField] private Transform objectPosition;
+
+    private void Update()
+    {
+        Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 futurePos = new Vector2(objectPosition.position.x, objectPosition.position.y); 
+
+        if (currentPos != futurePos)
+        {
+            transform.position = new Vector3(futurePos.x, futurePos.y, transform.position.z);
+        }
+    }
+}
---
+++ Tech Demo 1/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Player Controller:
+/// - In-charge of controlling the following actions:
+///   1. Move
+///   2. Jump
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private BoxCollider2D boxCollider;
+    private Animator animator;
+    private bool isFacingRight = true;
+
+    [Header("Move Settings:")]
+    [SerializeField] private float movementSpeed;
+    private float horizontalInput = 0.0f;
+
+    [Header("Jump Settings:")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float fallingGravity;
+    [SerializeField] private float castLength;
+    [SerializeField] private LayerMask layersToHit;
+    private float normalGravity;
+
+    //====================================================================================================
+    //-----------------------------------------Built-in Functions-----------------------------------------
+    //====================================================================================================
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+
+        normalGravity = rb2D.gravityScale;
+    }
+
+    private void Update()
+    {
+        GetHorizontalInput();
+        SetFacingDirection(horizontalInput);
+        UpdateAnimations();
+
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    //====================================================================================================
+    //-------------------------------------------Move Functions-------------------------------------------
+    //====================================================================================================
+    private void Move()
+    {
+        rb2D.velocity = new Vector2(horizontalInput * movementSpeed, rb2D.velocity.y);
+    }
+
+
+    //====================================================================================================
+    //-------------------------------------------Jump Functions-------------------------------------------
+    //====================================================================================================
+    private void Jump()
+    {
+        // INFO: Prevents the player from jumping in mid-air
+        if (IsGrounded())
+        {
+            rb2D.gravityScale = normalGravity;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+                animator.SetBool("IsJumping", true);
+            }
+        }
+
+        // INFO: Variable jump height made possible when 'space' key is
+        // released prematurely whilst the player is still moving upwards
+        if (Input.GetKeyUp(KeyCode.Space) && rb2D.velocity.y > 0)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, 0);
+            rb2D.gravityScale = fallingGravity;
+
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", true);
+        }
+        else if (rb2D.velocity.y < 0)
+        {
+            rb2D.gravityScale = fallingGravity;
+
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", true);
+        }
+    }
+
+
+    //====================================================================================================
+    //------------------------------------------Helper Functions------------------------------------------
+    //====================================================================================================
+    private void GetHorizontalInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private bool IsGrounded()
+    {
+        animator.SetBool("IsJumping", false);
+        animator.SetBool("IsFalling", false);
+
+        return Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0.0f, Vector2.down, castLength, layersToHit);
+    }
+
+    private void SetFacingDirection(float facingDirection)
+    {
+        // INFO: Changes the direction that the player is currently facing,
+        // however it only does this once, so as to prevent assigning the same
+        // values to it every time the function, as this is just unnecessary 
+        if (!isFacingRight && facingDirection > 0)
+        {
+            isFacingRight = true;
+            transform.localScale *= new Vector2(-1, 1);
+        }
+        else if (isFacingRight && facingDirection < 0)
+        {
+            isFacingRight = false;
+            transform.localScale *= new Vector2(-1, 1);
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        animator.SetFloat("MovementSpeed", Mathf.Abs(horizontalInput));
+    }
+}
